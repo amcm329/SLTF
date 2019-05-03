e085ade55d756cca38b243f2f458daa3b42614a5
Aaron <amcm329@hotmail.com>
2019-05-03 14:00:06 -0500

    Aaron's code part's test

--- TestCyclicSubSeriesSmoother.py
+++ TestCyclicSubSeriesSmoother.py
@@ -1,127 +1,128 @@
-import static com.github.servicenow.ds.stats.stl.CyclicSubSeriesSmoother.Builder;
-import math.pi as PI
+from CyclicSubSeriesSmoother import CyclicSubSeriesSmoother
+import math
+import unittest
 import numpy as np
+import math.pi as PI
+
 
 class CyclicSubSeriesSmootherTest:
 
       #Smoothing the cyclic sub-series extends the data one period in each direction. Ensure that when the data is 
       #linear, that the extrapolations are linear.
-      def TrendingSinusoidExtrapolationTest(self) {
-          period = 24;
-          double[] data = new double[2 * period]
+      def TrendingSinusoidExtrapolationTest(self):
+          period = 24
+          data = np.array(2 * period)
           dx = 2 * PI / period
-          for i in range (0, len(data):
-              i += 1
-              final int amplitude = 10 - i / period;
-              data[i] = amplitude * Math.sin(i * dx);
+          for i in range (len(data)):
+              amplitude = int(10 - i / period)
+              data[i] = float(amplitude * math.sin(i * dx))
 
-          double[] extendedData = new double[4 * period];
-          Builder builder = new Builder();
-          builder.setWidth(7); // Sub-cycle data is linear so width shouldn't matter
-          CyclicSubSeriesSmoother sssmoother = builder.setDataLength(data.length).setPeriodicity(period).extrapolateForwardAndBack(1).build();
+          extendedData = np.array(4 * period)
+          builder = Builder().setWidth(7) #Sub-cycle data is linear so width shouldn't matter
+          sssmoother = builder.setDataLength(data.length).setPeriodicity(period).extrapolateForwardAndBack(1).build()
+          sssmoother.smoothSeasonal(data, extendedData, null)
+
+          for i in range(len(extendedData)):
+              amplitude = int(11 - i / period) #An extra for the extrapolation before.
+              value = float(amplitude * math.sin(i * dx))
+              #assertEquals(String.format("test point %d", i), value, extendedData[i], 1.0e-11);
+
+
+      @Test
+      def shouldExtrapolateFourPeriodsForwards(self):
+          period = 24
+          data = np.array(2 * period) 
+          dx = 2 * PI / period
+          for i in range (len(data)):
+              amplitude = int(10 - i / period)
+              data[i] = float(amplitude * math.sin(i * dx))
+
+          extendedData = np.array(6 * period)
+          builder = Builder()
+          builder = builder.setWidth(7) #Sub-cycle data is linear so width shouldn't matter
+          builder = builder.extrapolateForwardOnly(4)
+          sssmoother = builder.setDataLength(data.length).setPeriodicity(period).build();
 
           sssmoother.smoothSeasonal(data, extendedData, null);
 
-          for (int i = 0; i < extendedData.length; ++i) {
-              final int amplitude = 11 - i / period; // An extra for the extrapolation before.
-              final double value = amplitude * Math.sin(i * dx);
-			assertEquals(String.format("test point %d", i), value, extendedData[i], 1.0e-11);
-		}
-	}
+          for i in range(len(extendedData)):
+              amplitude = int(10 - i / period)
+              value = float(amplitude * math.sin(i * dx))
+              #assertEquals(String.format("test point %d", i), value, extendedData[i], 1.0e-11);
 
-	@Test
-	public void shouldExtrapolateFourPeriodsForwards() {
-		final int period = 24;
-		double[] data = new double[2 * period];
-		final double dx = 2 * PI / period;
-		for (int i = 0; i < data.length; ++i) {
-			final int amplitude = 10 - i / period;
-			data[i] = amplitude * Math.sin(i * dx);
-		}
 
-		double[] extendedData = new double[6 * period];
+      @Test
+      def shouldExtrapolateTwoPeriodsBackwardAndTwoPeriodsForward(self):
+          period = 24
+          data = np.array([2 * period])
+          dx = float(2 * PI / period)
 
-		Builder builder = new Builder();
-		builder = builder.setWidth(7); // Sub-cycle data is linear so width shouldn't matter
-		builder = builder.extrapolateForwardOnly(4);
+          for i in range (len(data)):
+              amplitude = int(10 - i / period)
+              data[i] = float(amplitude * math.sin(i * dx))
 
-		CyclicSubSeriesSmoother sssmoother = builder.setDataLength(data.length).setPeriodicity(period).build();
+          extendedData = np.array(6 * period)
+          builder = Builder()
+          builder = builder.setWidth(7) #Sub-cycle data is linear so width shouldn't matter
+          builder = builder.setNumPeriodsForward(2).setNumPeriodsBackward(2)
+             
+          sssmoother = builder.setDataLength(data.length).setPeriodicity(period).build()
+ 
+          sssmoother.smoothSeasonal(data, extendedData, null)
 
-		sssmoother.smoothSeasonal(data, extendedData, null);
+          for i in range(len(extendedData)):
+              amplitude = int(12 - i / period) #Two extra for the extrapolation before.
+              value = float(amplitude * math.sin(i * dx))
+              #assertEquals(String.format("test point %d", i), value, extendedData[i], 1.0e-11);
 
-		for (int i = 0; i < extendedData.length; ++i) {
-			final int amplitude = 10 - i / period;
-			final double value = amplitude * Math.sin(i * dx);
-			assertEquals(String.format("test point %d", i), value, extendedData[i], 1.0e-11);
-		}
-	}
 
-	@Test
-	public void shouldExtrapolateTwoPeriodsBackwardAndTwoPeriodsForward() {
-		final int period = 24;
-		double[] data = new double[2 * period];
-		final double dx = 2 * PI / period;
-		for (int i = 0; i < data.length; ++i) {
-			final int amplitude = 10 - i / period;
-			data[i] = amplitude * Math.sin(i * dx);
-		}
+      def degreeMustBePositive(self):
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setDegree(-1)
+	
 
-		double[] extendedData = new double[6 * period];
+      def degreeMustBeLessThanThree(self): 
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setDegree(3)
+	
 
-		Builder builder = new Builder();
-		builder = builder.setWidth(7); // Sub-cycle data is linear so width shouldn't matter
-		builder = builder.setNumPeriodsForward(2).setNumPeriodsBackward(2);
+      def widthMustBeSet(self): 
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setDataLength(100).extrapolateForwardAndBack(1).setPeriodicity(12).build()
 
-		CyclicSubSeriesSmoother sssmoother = builder.setDataLength(data.length).setPeriodicity(period).build();
+    
+      def dataLengthMustBeSet(self):
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setWidth(3).extrapolateForwardAndBack(1).setPeriodicity(12).build()
 
-		sssmoother.smoothSeasonal(data, extendedData, null);
 
-		for (int i = 0; i < extendedData.length; ++i) {
-			final int amplitude = 12 - i / period; // Two extra for the extrapolation before.
-			final double value = amplitude * Math.sin(i * dx);
-			assertEquals(String.format("test point %d", i), value, extendedData[i], 1.0e-11);
-		}
-	}
+      def periodMustBeSet(self):
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setDataLength(100).extrapolateForwardAndBack(1).setWidth(11).build()
+	
 
-	@Test(expected = IllegalArgumentException.class)
-	public void degreeMustBePositive() {
-		Builder builder = new Builder();
-		builder.setDegree(-1);
-	}
+      def backwardExtrapolationMustBeSet(self):
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setDataLength(100).setNumPeriodsForward(1).setWidth(11).setPeriodicity(12).build()
 
-	@Test(expected = IllegalArgumentException.class)
-	public void degreeMustBeLessThanThree() {
-		Builder builder = new Builder();
-		builder.setDegree(3);
-	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void widthMustBeSet() {
-		Builder builder = new Builder();
-		builder.setDataLength(100).extrapolateForwardAndBack(1).setPeriodicity(12).build();
-	}
+      def forwardExtrapolationMustBeSet(self):
+          with self.assertRaises(ValueError):
+               builder = Builder()
+               builder.setDataLength(100).setNumPeriodsBackward(1).setWidth(11).setPeriodicity(12).build()
 
-	@Test(expected = IllegalArgumentException.class)
-	public void dataLengthMustBeSet() {
-		Builder builder = new Builder();
-		builder.setWidth(3).extrapolateForwardAndBack(1).setPeriodicity(12).build();
-	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void periodMustBeSet() {
-		Builder builder = new Builder();
-		builder.setDataLength(100).extrapolateForwardAndBack(1).setWidth(11).build();
-	}
+      def test___str__Test(self):
+          """ generated source for method toStringTest """
+          settings = LoessSettings(23)
+          str_ = settings.__str__()
+          self.assertEqual("[width = 23, degree = 1, jump = 3]", str_)
 
-	@Test(expected = IllegalArgumentException.class)
-	public void backwardExtrapolationMustBeSet() {
-		Builder builder = new Builder();
-		builder.setDataLength(100).setNumPeriodsForward(1).setWidth(11).setPeriodicity(12).build();
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void forwardExtrapolationMustBeSet() {
-		Builder builder = new Builder();
-		builder.setDataLength(100).setNumPeriodsBackward(1).setWidth(11).setPeriodicity(12).build();
-	}
-}
+if __name__ == '__main__':
+    unittest.main()
