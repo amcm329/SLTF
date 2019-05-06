bfe0187da6e04b7ca4e72979ab3b3a5e134a1a3b
delfoman <delfoman@hotmail.com>
2019-05-06 13:47:01 -0500

    Fix some sintaxis errors

--- TestCyclicSubSeriesSmoother.py
+++ TestCyclicSubSeriesSmoother.py
@@ -9,108 +9,108 @@ class CyclicSubSeriesSmootherTest(unittest.TestCase):
       #linear, that the extrapolations are linear.
       def test_TrendingSinusoidExtrapolationTest(self):
           period = 24
-          data = np.array(2 * period)
+          data = np.empty(2 * period)
           dx = 2 * math.pi / period
-          for i in range (len(data)):
+          for i in range(len(data)):
               amplitude = int(10 - i / period)
               data[i] = float(amplitude * math.sin(i * dx))
 
-          extendedData = np.array(4 * period)
-          builder = Builder().setWidth(7) #Sub-cycle data is linear so width shouldn't matter
-          sssmoother = builder.setDataLength(data.length).setPeriodicity(period).extrapolateForwardAndBack(1).build()
-          sssmoother.smoothSeasonal(data, extendedData, null)
+          extendedData = np.empty(4 * period)
+          builder = CyclicSubSeriesSmoother.Builder().setWidth(7) #Sub-cycle data is linear so width shouldn't matter
+          sssmoother = builder.setDataLength(len(data)).setPeriodicity(period).extrapolateForwardAndBack(1).build()
+          sssmoother.smoothSeasonal(data, extendedData, None)
 
           for i in range(len(extendedData)):
               amplitude = int(11 - i / period) #An extra for the extrapolation before.
               value = float(amplitude * math.sin(i * dx))
-              self.assertAlmostEqual(value, extendedData[i], 1.0e-11, "test point {0}".format(i))
+              self.assertAlmostEqual(value, extendedData[i],  msg="test point {0}".format(i), delta=1.0e-11)
 
 
       def test_shouldExtrapolateFourPeriodsForwards(self):
           period = 24
-          data = np.array(2 * period) 
+          data = np.empty(2 * period) 
           dx = 2 * math.pi / period
-          for i in range (len(data)):
+          for i in range(len(data)):
               amplitude = int(10 - i / period)
               data[i] = float(amplitude * math.sin(i * dx))
 
-          extendedData = np.array(6 * period)
-          builder = Builder()
+          extendedData = np.empty(6 * period)
+          builder = CyclicSubSeriesSmoother.Builder()
           builder = builder.setWidth(7) #Sub-cycle data is linear so width shouldn't matter
           builder = builder.extrapolateForwardOnly(4)
-          sssmoother = builder.setDataLength(data.length).setPeriodicity(period).build();
+          sssmoother = builder.setDataLength(len(data)).setPeriodicity(period).build();
 
-          sssmoother.smoothSeasonal(data, extendedData, null);
+          sssmoother.smoothSeasonal(data, extendedData, None);
 
           for i in range(len(extendedData)):
               amplitude = int(10 - i / period)
               value = float(amplitude * math.sin(i * dx))
-              self.assertEquals(value, extendedData[i], 1.0e-11,"test point {0}".format(i))
+              self.assertAlmostEqual(value, extendedData[i], msg="test point {0}".format(i), delta=1.0e-11)
 
 
       def test_shouldExtrapolateTwoPeriodsBackwardAndTwoPeriodsForward(self):
           period = 24
-          data = np.array([2 * period])
+          data = np.empty(2 * period)
           dx = float(2 * math.pi / period)
 
           for i in range (len(data)):
               amplitude = int(10 - i / period)
               data[i] = float(amplitude * math.sin(i * dx))
 
-          extendedData = np.array(6 * period)
-          builder = Builder()
+          extendedData = np.empty(6 * period)
+          builder = CyclicSubSeriesSmoother.Builder()
           builder = builder.setWidth(7) #Sub-cycle data is linear so width shouldn't matter
           builder = builder.setNumPeriodsForward(2).setNumPeriodsBackward(2)
              
-          sssmoother = builder.setDataLength(data.length).setPeriodicity(period).build()
+          sssmoother = builder.setDataLength(len(data)).setPeriodicity(period).build()
  
-          sssmoother.smoothSeasonal(data, extendedData, null)
+          sssmoother.smoothSeasonal(data, extendedData, None)
 
           for i in range(len(extendedData)):
               amplitude = int(12 - i / period) #Two extra for the extrapolation before.
               value = float(amplitude * math.sin(i * dx))
-              self.assertEquals(value, extendedData[i], 1.0e-11,"test point {0}".format(i))
+              self.assertAlmostEqual(value, extendedData[i], msg="test point {0}".format(i), delta=1.0e-11)
 
 
       def test_degreeMustBePositive(self):
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setDegree(-1)
 	
 
       def test_degreeMustBeLessThanThree(self): 
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setDegree(3)
 	
 
       def test_widthMustBeSet(self): 
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setDataLength(100).extrapolateForwardAndBack(1).setPeriodicity(12).build()
 
     
       def test_dataLengthMustBeSet(self):
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setWidth(3).extrapolateForwardAndBack(1).setPeriodicity(12).build()
 
 
       def test_periodMustBeSet(self):
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setDataLength(100).extrapolateForwardAndBack(1).setWidth(11).build()
 	
 
       def test_backwardExtrapolationMustBeSet(self):
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setDataLength(100).setNumPeriodsForward(1).setWidth(11).setPeriodicity(12).build()
 
 
       def test_forwardExtrapolationMustBeSet(self):
           with self.assertRaises(ValueError):
-               builder = Builder()
+               builder = CyclicSubSeriesSmoother.Builder()
                builder.setDataLength(100).setNumPeriodsBackward(1).setWidth(11).setPeriodicity(12).build()
 
 
